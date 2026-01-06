0754a41e39e0666e9fd98ab14e6934b01018e616
theCodeForgerHQ <ajayaditya.dev@gmail.com>
2026-01-06 18:49:50 +0530

    setup dev scripts and hot reload

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,15 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./utils.js";
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
   });
-
-  mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   build: {
     outDir: "dist-react",
   },
+  server: {
+    port: 5123,
+    strictPort: true,
+  },
 });
