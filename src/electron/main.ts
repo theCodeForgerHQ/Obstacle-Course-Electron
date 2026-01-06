fa9927223b3a030159f3f3ed54158c3061b92c6c
theCodeForgerHQ <ajayaditya.dev@gmail.com>
2026-01-06 18:24:58 +0530

    initial commit

---
+++ src/electron/main.ts
@@ -0,0 +1,11 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+app.whenReady().then(() => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+});
